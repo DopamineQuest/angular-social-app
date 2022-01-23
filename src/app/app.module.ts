a30e3fee6493b29d3efb3e8b3f246e7c370770b9
“Antonin <dopaminequestgames@gmail.com>
2022-01-22 23:49:39 -0600

    add main page and home page, add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+    { path: 'main', component: MainPageComponent }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
